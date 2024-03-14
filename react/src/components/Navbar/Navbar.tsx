a839eec28ee1bac7fb8b3032dfd754a5f69b8d62
YuhriParada <yuhriparada@gmail.com>
2024-03-14 01:54:47 -0300

    navbar dropdown

--- react/src/components/Navbar/Navbar.tsx
+++ react/src/components/Navbar/Navbar.tsx
@@ -1,25 +1,46 @@
-/* import RubyLogo from '../../assets/logo/logo2.png'; */
-import React from 'react';
+import RubyLogo from '../../assets/logo/logo2.png';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import { CodeBracketSquareIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
+/* import { CodeBracketSquareIcon, ChevronDownIcon } from '@heroicons/react/24/outline'; */
+
+// const Navbar: React.FC = () => {
+//  return (
+//     <nav className={styles.navbar}>
+//       <div className="flex items-center">
+//         <CodeBracketSquareIcon className="h-5 w-5 text-white" aria-hidden="true" />
+//         <span className="ml-2 text-white">Logo</span>
+//       </div>
+//       <div className={styles.dropdown}>
+//         <button className={styles.dropdownButton}>Menu</button>
+//         <div className={styles.dropdownContent}>
+//           <a href="#">Link 1</a>
+//           <a href="#">Link 2</a>
+//           <a href="#">Link 3</a>
+//         </div>
+//       </div>
+//     </nav>
+//  );
+// };
+//
+// export default Navbar;
+
 const Navbar: React.FC = () => {
- return (
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
     <nav className={styles.navbar}>
       <div className="flex items-center">
-        <CodeBracketSquareIcon className="h-5 w-5 text-white" aria-hidden="true" />
-        <span className="ml-2 text-white">Logo</span>
+        <img src={RubyLogo} className={`${styles.logo}`} alt="Logo" />
       </div>
       <div className={styles.dropdown}>
-        <button className={styles.dropdownButton}>Menu</button>
-        <div className={styles.dropdownContent}>
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <button className={styles.dropdownButton} onClick={() => setIsOpen(!isOpen)}>Menu</button>
+        <div className={`${styles.dropdownContent} ${isOpen ? styles.show : ''}`}>
+          <a href="#">Projects</a>
+          <a href="#">Contact Me</a>
         </div>
       </div>
     </nav>
- );
+  );
 };
 
 export default Navbar;
-
