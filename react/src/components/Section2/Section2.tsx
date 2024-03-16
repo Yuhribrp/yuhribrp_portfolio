3e6768e9cf94afa4086287bf81f240795dac15b8
YuhriParada <yuhriparada@gmail.com>
2024-03-16 12:08:28 -0300

    Section2 style and animitaions

--- react/src/components/Section2/Section2.tsx
+++ react/src/components/Section2/Section2.tsx
@@ -5,21 +5,29 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from './Section2.module.css';
 import cardBg1 from '../../assets/cards/card-bg1.jpg';
 import cardBg2 from '../../assets/cards/card-bg2.jpg';
+import cardBg3 from '../../assets/cards/card-bg3.jpg';
+import cardBg4 from '../../assets/cards/card-bg4.jpg';
 
 const slides = [
   {
-    title: "Machu Picchu",
-    subtitle: "Peru",
-    description: "Adventure is never far away",
+    imageName: "Rolê Canino",
     image: cardBg1,
-    imageName: "Rolê Canino"
+    link: "#"
   },
   {
-    title: "Chamonix",
-    subtitle: "France",
-    description: "Let your dreams come true",
+    imageName: "Receipts Api",
     image: cardBg2,
-    imageName: "Receipts Api"
+    link: "#"
+  },
+  {
+    imageName: "Cryptofolio",
+    image: cardBg3,
+    link: "#"
+  },
+  {
+    imageName: "Torcida na Rede",
+    image: cardBg4,
+    link: "#"
   }
 ];
 
@@ -33,7 +41,7 @@ function useTilt() {
       const { left, top, width, height } = tiltElement.getBoundingClientRect();
       const x = (event.pageX - left) / width - 0.5;
       const y = (event.pageY - top) / height - 0.5;
-      tiltElement.style.transform = `rotateX(${-y * 15}deg) rotateY(${x * 15}deg)`;
+      tiltElement.style.transform = `rotateX(${-y * 10}deg) rotateY(${x * 10}deg)`;
     };
     tiltElement.addEventListener('mousemove', handleMouseMove);
     return () => tiltElement.removeEventListener('mousemove', handleMouseMove);
@@ -46,24 +54,30 @@ function Slide({ slide }: { slide: typeof slides[0] }) {
   const tiltRef = useTilt();
   return (
     <div className={styles.slide} ref={tiltRef}>
-      <div
+      {/* <div
         className={styles.slideBackground}
         style={{
           backgroundImage: `url('${slide.image}')`
         }}
-      />
+      /> */}
       <div
         className={styles.slideContent}
         style={{
           backgroundImage: `url('${slide.image}')`
         }}
       >
-        <div className={styles.slideContentInner}>
+        {/* <div className={styles.slideContentInner}>
           <h2 className={styles.slideTitle}>{slide.title}</h2>
           <h3 className={styles.slideSubtitle}>{slide.subtitle}</h3>
           <p className={styles.slideDescription}>{slide.description}</p>
-        </div>
-        <p className={styles.slideImageName}>{slide.imageName}</p>
+        </div> */}
+        <a
+          href={slide.link}
+          className={styles.slideImageName}
+          onMouseEnter={(e) => e.currentTarget.classList.add('animate')}
+          onMouseLeave={(e) => e.currentTarget.classList.remove('animate')}
+        >
+          {slide.imageName}</a>
       </div>
     </div>
   );
