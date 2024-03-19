8e49ee0de4d8ab087788a544cb59e3215d10b069
YuhriParada <yuhriparada@gmail.com>
2024-03-19 02:10:29 -0300

    built front env vite defined

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL || 'https://yuhribrp-portfolio.osc-fr1.scalingo.io';
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL || 'http://127.0.0.1:3000';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
