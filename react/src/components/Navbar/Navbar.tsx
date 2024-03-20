bb40720a84350b155ec4213211a9b845be945401
YuhriParada <yuhriparada@gmail.com>
2024-03-20 14:56:03 -0300

    feat(Front):(improved navbar)

--- react/src/components/Navbar/Navbar.tsx
+++ react/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import styles from './Navbar.module.css';
-import YhLogo from '../../assets/logo/logoyh.png';
+// import YhLogo from '../../assets/logo/logoyh.png';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [rotation, setRotation] = useState(0);
+  // const [rotation, setRotation] = useState(0);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setRotation(rotation => rotation + Math.random() * 360 - 180);
-    }, 1000);
-    return () => clearInterval(interval);
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 50;
+      setIsScrolled(isScrolled);
+    };
+
+    document.addEventListener('scroll', handleScroll);
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
   }, []);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setRotation(rotation => rotation + Math.random() * 360 - 180);
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   useEffect(() => {
     const handleClickOutside = (event: { target: unknown; }) => {
@@ -46,16 +59,22 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${isScrolled ? styles.scrolled : ''}`}>
       <div className="flex items-center">
-        <img src={YhLogo} className={`${styles.logo}`} alt="Logo" style={{ transform: `rotate(${rotation}deg)` }} />
       </div>
       <div className={styles.dropdown} ref={dropdownRef}>
-        <button className={styles.dropdownButton} onClick={() => setIsOpen(!isOpen)}>Menu</button>
-        <div className={`${styles.dropdownContent} ${isOpen ? styles.show : ''}`}>
-          <a href="#section2" onClick={() => setIsOpen(false)}>Projects</a>
-          <a href="#" onClick={fetchResume}>Resume</a>
-        </div>
+        <button className={`${styles.dropdownButton} ${isScrolled ? 'bg-black text-white' : 'bg-transparent text-white'}`} onClick={() => setIsOpen(!isOpen)}>
+          Menu
+          <svg className={`${isOpen ? 'transform rotate-180' : ''} ml-2 ${styles.arrowDown}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+          </svg>
+        </button>
+        {isOpen && (
+          <div className={`${styles.dropdownContent}`}>
+            <a href="#section2" onClick={() => setIsOpen(false)}>Projects</a>
+            <a href="#" onClick={fetchResume}>Resume</a>
+          </div>
+        )}
       </div>
     </nav>
   );
