bbee50fc46576afe9ebda3f38309eb76718a8fe6
YuhriParada <yuhriparada@gmail.com>
2024-04-02 11:06:27 -0300

    components organized

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
-import About from './components/About';
+import Header from './components/Header/Header';
+import About from './components/About/About';
 import Section2 from './components/Section2/Section2'
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 
 export type PortfolioOwner = {
   id: number,
--- react/src/components/Footer/Footer.tsx
+++ react/src/components/Footer/Footer.tsx
@@ -1,30 +1,30 @@
-// import styles from './Footer.module.css';
-// import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
-// import Dino from '../../assets/logo/dino3.gif';
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
 
-// const Footer = () => {
-//   return (
-//     <footer className={styles.footer}>
-//       <div className={styles.container}>
-//         <div className={styles.folio}>© 2024 Portfolio</div>
-//         <div className={styles.email}>yuhribrp.dev@gmail.com</div>
-//         <div className={styles.icons}>
-//           <a href="https://twitter.com/Yuhribrp" target="_blank" rel="noopener noreferrer">
-//             <FaTwitter size={30} />
-//           </a>
-//           <a href="https://github.com/Yuhribrp" target="_blank" rel="noopener noreferrer">
-//             <FaGithub size={30} />
-//           </a>
-//           <a href="https://www.linkedin.com/in/yuhribrp" target="_blank" rel="noopener noreferrer">
-//             <FaLinkedin size={30} />
-//           </a>
-//         </div>
-//         <div className={styles.dino}>
-//           <img src={Dino} alt="Dino walking" />
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
+const Footer = () => {
+  const year = new Date().getFullYear();
 
-// export default Footer;
+  return (
+    <footer className="light-bg flex justify-space">
+      <span className="default">© {year} @Yuhribrp</span>
+      <ul className="flex">
+        <li>
+          <a href="https://www.linkedin.com/in/yuhribrp" className="white"  target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={30} />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Yuhribrp" className="white"  target="_blank" rel="noopener noreferrer">
+            <FaGithub size={30} />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/Yuhribrp" className="white"  target="_blank" rel="noopener noreferrer">
+            <FaTwitter size={30} />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
