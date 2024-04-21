624ec1eb92eb2db18860f37e6febeac64c89840c
YuhriParada <yuhriparada@gmail.com>
2024-04-21 19:07:13 -0300

    puma and procfile updated

--- Procfile
+++ Procfile
@@ -1,5 +1,4 @@
 # rails: bundle exec rails s -p 3000
 # web: npm run dev
 # web: rake db:migrate && bin/rails server -b 0.0.0.0 -p $PORT
-
-web: rake db:migrate && bundle exec rails server -b 0.0.0.0 -p ${PORT:-5000}
+web: bundle exec rails server -b 0.0.0.0 -p ${PORT:-5000}
--- config/puma.rb
+++ config/puma.rb
@@ -11,12 +11,19 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+pidfile_path = ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+if File.exist?(pidfile_path)
+  require 'fileutils'
+  FileUtils.rm(pidfile_path)
+end
+
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
   workers worker_count if worker_count > 1
-  worker_timeout 7200
+  worker_timeout 3600
   preload_app!
   on_worker_boot do
     ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
@@ -34,7 +41,7 @@ port ENV.fetch("PORT") { 5000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
